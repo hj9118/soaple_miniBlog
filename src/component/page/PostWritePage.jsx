b680bbbb3d4023d41d12e90ce5144187097be323
jiho9118 <mexocoxon@gmail.com>
2023-01-10 16:42:21 +0900

    코드 수정

--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -1,35 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
 
 const Wrapper = styled.div`
+    padding: 16px;
     width: calc(100% - 32px);
-    padding: 8px 16px;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
     justify-content: center;
-    border: 1px solid grey;
-    border-radius: 8px;
-    cursor: pointer;
-    background: white;
-    :hover {
-        background: lightgrey;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
     }
 `;
 
-const ContentText = styled.p`
-    font-size: 16px;
-    white-space: pre-wrap;
-`;
+function PostWritePage(props) {
+    const navigate = useNavigate();
 
-function CommentListItem(props) {
-    const { comment } = props;
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
     return (
         <Wrapper>
-            <ContentText>{comment.content}</ContentText>
+            <Container>
+                <TextInput
+                    height={20}
+                    value={title}
+                    onChange={(event) => {
+                        setTitle(event.target.value);
+                    }}
+                />
+
+                <TextInput
+                    height={480}
+                    value={content}
+                    onChange={(event) => {
+                        setContent(event.target.value);
+                    }}
+                />
+
+                <Button
+                    title="글 작성하기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+            </Container>
         </Wrapper>
     );
 }
 
-export default CommentListItem;
+export default PostWritePage;
