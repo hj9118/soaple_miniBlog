5f42e0a0a8de0b6cc0ef5bfb2219aa0e7a0cf90e
jiho9118 <mexocoxon@gmail.com>
2023-01-10 13:49:30 +0900

    확장자명 변경 및 코드 컨벤션 조정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
-import MainPage from './component/page/Mainpage';
+import MainPage from './component/page/MainPage';
 import PostWritePage from './component/page/PostWritePage';
 import PostViewPage from './component/page/PostViewPage';
 
@@ -17,7 +17,7 @@ function App(props) {
     <BrowserRouter>
       <MainTitleText>미니 블로그</MainTitleText>
       <Routes>
-        <Route index element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path='post-write' element={<PostWritePage />} />
         <Route path='post/:postId' element={<PostViewPage />} />
       </Routes>
